2332bc77c50f526ce8a906fa82a9c36a7fdeecc3
Marwa <marwa.niaaj@gmail.com>
2022-08-12 12:51:49 +0400

    Initial Commit

---
+++ UnitConversionApp/UnitConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionAppApp.swift
+//  UnitConversionApp
+//
+//  Created by Marwa Abou Niaaj on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
